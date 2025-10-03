4d294902aa4b0460b3361e6334a4d83598a90d85
Brian Walshe <walshe.brian@gmail.com>
2025-10-03 16:23:44 +0100

    changed logging so that each battle gets saved to a different file

--- src/zerosumfc/battle.py
+++ src/zerosumfc/battle.py
@@ -1,10 +1,19 @@
-import click
-import enum
+from datetime import datetime
 import logging
+import pathlib
+import click
 
 from zerosumfc.buckshotroulette import Game
 from zerosumfc.cliutils import AgentChoice, make_agent
 from zerosumfc.data import Role
+
+
+logger = logging.getLogger(__name__)
+
+
+def print_and_log(msg: str):
+    logger.info(msg)
+    print(msg)
 
 
 @click.command()
@@ -13,12 +22,24 @@ from zerosumfc.data import Role
 @click.option("--rounds", type=int, default=10)
 @click.option("--health", type=int, default=5)
 def main(player_agent, dealer_agent, rounds, health):
+    player_name = player_agent.name
+    dealer_name = dealer_agent.name
+
+    timestamp = datetime.now().strftime("%Y.%m.%d-%H%M%S")
+
+    log_dir = pathlib.Path("./logs")
+    log_dir.mkdir(exist_ok=True)
+    logfile = f"{player_name}_vs_{dealer_name}_{timestamp}.log"
     logging.basicConfig(
         format="%(asctime)s %(levelname)s: %(message)s",
-        filename="battle.log",
+        filename=log_dir / logfile,
         encoding="utf-8",
         level=logging.INFO,
     )
+    print_and_log(
+        f"Evaluating player: {player_agent} vs dealer: {dealer_agent}"
+    )
+    logger.info(f"{rounds} rounds with initial health {health}")
     player = make_agent(player_agent, Role.PLAYER)
     dealer = make_agent(dealer_agent, Role.DEALER)
 
@@ -27,15 +48,19 @@ def main(player_agent, dealer_agent, rounds, health):
 
     for i in range(rounds):
         game = Game(dealer=dealer, player=player, initial_health=health)
-        print(f"Running game {i}")
+        print_and_log(f"Running game {i}")
         winner = game.run()
-        print(f"Winner is {winner}")
+        print_and_log(f"Winner is {winner}")
         match winner:
             case Role.PLAYER:
                 player_wins += 1
             case Role.DEALER:
                 dealer_wins += 1
 
+    logger.info(
+        f"Evaluation finished. Player won {player_wins} rounds and "
+        f"dealer won {dealer_wins} rounds"
+    )
     print("Results:")
     print(f"Player: {player_wins}")
     print(f"Dealer: {dealer_wins}")
